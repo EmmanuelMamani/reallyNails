fce437d7e1ed582b4fec2fc054c69150ef1f8866
Emmanuel Mamani <emmanuelmamani100@gmail.com>
2025-05-27 10:03:12 -0400

    mom day finish

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="p-4">
-    <h1 class="text-2xl font-bold mb-4 text-center">Really Nails</h1>
-    <h2 class="text-lg text-center" >Arte al alcance de tus manos</h2>
+  <div class="w-screen h-screen p-4 bg-slate-900">
+    <img src="/img/logoRn.png" class="w-full h-full object-contain" />
   </div>
 </template>
 
